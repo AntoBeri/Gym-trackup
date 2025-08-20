dd7af5ba9181990989e4602d06b5ff873e8a559c
Beri <antonio.beri.grc@gmail.com>
2025-08-19 18:02:49 -0600

    Created csv template, added creation of own database and modified .gitignore
    The template csv file to use in the creation of the personal database has been created. main file now trys to open the personal database and if not existing one creates it from the template. .gitignore was modified to avoid the personal database to be tracked by git.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import pandas as pd
+from pathlib import Path
+
+
 def main():
-    print('main function created')
+    # Load the template and create your own file
+    file = Path('Gym-trackup/data/tracks.csv')
+
+    if not file.exists():
+        template = pd.read_csv('Gym-trackup/data/template.csv')
+        tracks = pd.DataFrame(columns=template.columns)
+        tracks.to_csv('Gym-trackup/data/tracks.csv', index=False)
+    else:
+        tracks = pd.read_csv(file)
+
+    print(tracks.head)
 
 if __name__ == '__main__':
     main()
